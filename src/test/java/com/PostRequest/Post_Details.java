bab413bb5eb2472b52a88912da47852ca5b74467
08Aakansha <as08121996@gmail.com>
2021-05-29 18:49:59 +0530

    Added Pojo functionlity in Post details class

--- src/test/java/com/PostRequest/Post_Details.java
+++ src/test/java/com/PostRequest/Post_Details.java
@@ -6,8 +6,11 @@ import org.testng.annotations.Test;
 import com.testData.Payload;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
+import pojo.DeserializeJSON;
 
 public class Post_Details {
 	
@@ -17,8 +20,11 @@ public class Post_Details {
 		RestAssured.baseURI= "http://dummy.restapiexample.com";
 		//setting request specification class
 		 RequestSpecification httpRequest = RestAssured.given();
-		 Response response = httpRequest.body(payload.employeeDetailsPayload()).post("/api/v1/create");
-		System.out.println(response.asString());
+		 Response response = httpRequest. contentType(ContentType.JSON).body(payload.employeeDetailsPayload()).post("/api/v1/create");
+		 ResponseBody body = response.getBody();
+		 DeserializeJSON responseBody = body.as(DeserializeJSON.class);
+		 System.out.println(responseBody.getMessage());
+		 
 		//fdfdfdfdfdf
 
 	}	
--- src/test/java/pojo/DeserializeJSON.java
+++ src/test/java/pojo/DeserializeJSON.java
@@ -2,4 +2,25 @@ package pojo;
 
 public class DeserializeJSON {
   public String status;
+  public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+public Data getData() {
+	return data;
+}
+public void setData(Data data) {
+	this.data = data;
+}
+public String getMessage() {
+	return message;
+}
+public void setMessage(String message) {
+	this.message = message;
+}
+public Data data;
+  public String message;
+
 }
